14c0d8ce5122c6ca540399ef8be8ad1f7be9bce7
mogola sangare <mogola.sangare@gmail.com>
2020-06-02 19:11:39 +0200

    fix offline 200

--- src/index.js
+++ src/index.js
@@ -11,14 +11,4 @@ ReactDOM.render(<App />, document.getElementById("root"));
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: http://bit.ly/CRA-PWA
 
-if ('serviceWorker' in navigator) {
-    window.addEventListener('load', () => {
-        navigator.serviceWorker.register('/service-worker.js').then(registration => {
-            console.log('SW registered: ', registration);
-        }).catch(registrationError => {
-            console.log('SW registration failed: ', registrationError);
-        });
-    });
-}
-
 serviceWorker.register();
